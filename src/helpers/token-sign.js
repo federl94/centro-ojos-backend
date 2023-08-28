953248c3eca4465be74ea8c8bbd5f6f288f78c5d
Federico <federuedaleal@hotmail.com>
2023-08-28 00:01:07 -0300

    se modifica token.signs

--- src/helpers/token-sign.js
+++ src/helpers/token-sign.js
@@ -1,21 +1,32 @@
-import jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-const generarJWT = (nombreUsuario)=>{
-    return new Promise( (resolve, reject)=>{
-        //agregar los datos al payload
-        const payload = {nombreUsuario};
-        //aqui firmamos el token
-        jwt.sign(payload,process.env.SECRET_JWT,{
-            expiresIn: '2h'
-        },(err, token)=>{
-            if(err){
-                console.log(err);
-                reject('Error al generar el token')
-            }
-            //si esta todo correcto
-            resolve(token);
-        })
-    })
-}
+const generarJWT = (nombreUsuario) => {
+  return new Promise((resolve, reject) => {
+    // Agregar los datos al payload
+    const payload = { nombreUsuario };
 
-export default generarJWT
+    // Imprimir el payload y el valor de SECRET_JWT
+    console.log("Payload:", payload);
+    console.log("SECRET_JWT:", process.env.SECRET_JWT);
+
+    // Aquí firmamos el token
+    jwt.sign(
+      payload,
+      process.env.SECRET_JWT,
+      {
+        expiresIn: '2h'
+      },
+      (err, token) => {
+        if (err) {
+          console.log(err);
+          reject('Error al generar el token');
+        }
+        // Si está todo correcto
+        console.log("Token generado:", token);
+        resolve(token);
+      }
+    );
+  });
+};
+
+export default generarJWT;
