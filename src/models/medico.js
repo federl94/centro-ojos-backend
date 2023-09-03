0616c321092a273d6440fae65f58b67950e059af
Federico <federuedaleal@hotmail.com>
2023-09-02 23:53:56 -0300

    Se agrega Medico al proyecto

---
+++ src/models/medico.js
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+
+const medicoSchema = new Schema({
+    nombreMedico: {
+        type: String,
+        minLength: 2,
+        maxLength: 50,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (valor) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(valor);
+            }
+        }
+    },
+    telefono: {
+        type: Number,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (valor) {
+                return /^[0-9]{10}$/.test(valor);
+            }
+        }
+    },
+    especialidad: {
+        type: String,
+        minLength: 3,
+        maxLength: 40,
+        required: true
+    },
+    obrasSociales: {
+        type: String,
+        minLength: 3,
+        maxLength: 200,
+        required: true
+    }
+})
+
+const Medico = model('medico', medicoSchema);
+
+export default Medico;
