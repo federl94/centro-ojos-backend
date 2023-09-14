b463fc635972b3b44003fa08d9526d6b750a01a5
Federico <federuedaleal@hotmail.com>
2023-09-14 17:41:20 -0300

    Se agrega el campo "diastrabajados" al modelo y validacion de medico

--- src/helpers/validacionMedico.js
+++ src/helpers/validacionMedico.js
@@ -1,12 +1,12 @@
-import { check } from "express-validator";
+import { check, isArray } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion";
 
 const validarMedico = [
     check("nombreMedico")
     .notEmpty()
     .withMessage("El nombre del médico es obligatorio")
-    .isLength({min: 2, max: 50})
-    .withMessage("El nombre debe tener entre 2 y 50 carateres"),
+    .isLength({ min: 2, max: 50 })
+    .withMessage("El nombre debe tener entre 2 y 50 caracteres"),
     check("email")
     .notEmpty()
     .withMessage("El email es obligatorio")
@@ -14,18 +14,27 @@ const validarMedico = [
     .withMessage("El mail debe cumplir el formato nombre@dominio.com"),
     check("telefono")
     .notEmpty()
-    .withMessage("El telefono del médico es obligatorio")
+    .withMessage("El teléfono del médico es obligatorio")
     .matches(/^[0-9]{10}$/)
-    .withMessage("El telefono debe tener 10 digitos"),
+    .withMessage("El teléfono debe tener 10 dígitos"),
     check("especialidad")
     .notEmpty()
     .withMessage("La especialidad es obligatoria")
-    .isLength({min: 3, max: 40})
-    .withMessage("La especialidad debe tener entre 3 y 40 carateres"),    
+    .isLength({ min: 3, max: 40 })
+    .withMessage("La especialidad debe tener entre 3 y 40 caracteres"),
     check("obrasSociales")
     .notEmpty()
-    .withMessage("Las obras sociales con las que trabaja es obligatoria"),
-    (req, res, next)=>{resultadoValidacion(req, res, next)}
-]
+    .withMessage("Las obras sociales con las que trabaja son obligatorias"),
+    check("diasTrabajo")
+    .custom((diasTrabajo) => {
+        if (!isArray(diasTrabajo)) {
+            throw new Error("Los días de trabajo deben ser un array");
+        }
+        return true;
+    }),
+    (req, res, next) => {
+        resultadoValidacion(req, res, next);
+    }
+];
 
 export default validarMedico;
